d052ae3f6ea10a3755670327b6d24aafb59e4aea
Kim Barcelo <42252631+kimbarcelo1@users.noreply.github.com>
2026-05-17 14:37:25 +0800

    Create repeatString function using for loop and conditional

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, nTimes) {
+    let repeatedText = "";
+    
+    if (nTimes < 0) {
+        return "ERROR";
+    }
+    
+    for (let i = 1; i <= nTimes; i++) {
+        repeatedText += str;
+    }
+    return repeatedText;
 };
 
 // Do not edit below this line
