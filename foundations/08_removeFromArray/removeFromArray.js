36264e60ff056eaca8e0c40995e810eaf4db470d
Kim Barcelo <42252631+kimbarcelo1@users.noreply.github.com>
2026-05-30 21:20:25 +0800

    Add comment about my buggy solution for improvement in the future

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -3,7 +3,7 @@ const removeFromArray = function(arr, ...items) {
     for (let i = 0; i < items.length; i++) {
         
         // loop through arr
-        for (let j = 0; j < arr.length; j++) {
+        for (let j = 0; j < arr.length; j++) { // there is a bug in this solution and j was never used. Remember to use filter for this task.
             const index = arr.indexOf(items[i]);
             
             if (index > -1) {
