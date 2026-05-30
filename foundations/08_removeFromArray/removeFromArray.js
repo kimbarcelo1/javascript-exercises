588503fe28a6e0f4888d10d35125785e74ab0727
Kim Barcelo <42252631+kimbarcelo1@users.noreply.github.com>
2026-05-30 21:13:41 +0800

    My solution for the removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    // loop through the rest parameter ...items
+    for (let i = 0; i < items.length; i++) {
+        
+        // loop through arr
+        for (let j = 0; j < arr.length; j++) {
+            const index = arr.indexOf(items[i]);
+            
+            if (index > -1) {
+                arr.splice(index, 1);
+            }
+        }
+    }
+    
+    return arr;
 };
 
 // Do not edit below this line
