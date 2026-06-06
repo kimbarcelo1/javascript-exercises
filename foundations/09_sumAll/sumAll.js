125a93e2eca3a1425f4eb5aa01798792ee4061f3
Kim Barcelo <42252631+kimbarcelo1@users.noreply.github.com>
2026-06-06 20:36:24 +0800

    My solution for the sumAll problem

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if (n1 <= 0 || n2 <= 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(n1) || !Number.isInteger(n2)) {
+        return "ERROR";
+    }
+    
+    let lowest = n1;
+    let highest = n2;
+    let sum = 0;
+    
+    if (n1 > n2) {
+        highest = n1;
+        lowest = n2;
+    }
+    
+    for (let i = lowest; i <= highest; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
