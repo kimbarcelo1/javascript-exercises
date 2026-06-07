7ce1fae7ccbc05f9a7a00570b312be8fdb839f5b
Kim Barcelo <42252631+kimbarcelo1@users.noreply.github.com>
2026-06-07 14:47:55 +0800

    Temp conversion formulas

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let result = (temp - 32) * 5/9;
+  result = Math.round(result * 10) / 10;
+
+  return Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = (temp * 9/5 + 32);
+  result = Math.round(result * 10) / 10;
+
+  return Number(result.toFixed(1));
 };
 
 // Do not edit below this line
