15ebebad1c68a49b8e7d942b9a43e1e3721ccb19
Kim Barcelo <42252631+kimbarcelo1@users.noreply.github.com>
2026-08-02 21:38:04 +0800

    My solution for the fibonacci problem.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // num = Number(num);
+    if (num < 0) {
+        return "OOPS";
+    }
 
+    if (num == 1) {
+        return 1;
+    }
+
+    let a = 0;
+    let b = 1;
+    let sum = 0;
+    for (let i = 1; i < num; i++) {
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
