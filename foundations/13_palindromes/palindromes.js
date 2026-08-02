14ab5108384725a26c59afb0994445d9101bbdb4
Kim Barcelo <42252631+kimbarcelo1@users.noreply.github.com>
2026-08-02 19:36:18 +0800

    Palindromes using array and string  methods.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    str = str.toLowerCase().replaceAll(",", "").replace(".", "").replaceAll("!", "").replaceAll(" ", "");
+    let reversedStr = str.split("").reverse().join("");
+    
+    // console.log(str, reversedStr);
+    return str === reversedStr;
 };
 
 // Do not edit below this line
