1a345948d1b59dfedc3a23c70e744344041e4ca6
Kim Barcelo <42252631+kimbarcelo1@users.noreply.github.com>
2026-08-03 21:07:19 +0800

    Array reduce to get the oldest person.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((acc, cur) => {
+       const year = new Date().getFullYear();
+       let age1 = (acc.yearOfDeath || year) - acc.yearOfBirth; 
+       let age2 = (cur.yearOfDeath || year) - cur.yearOfBirth;
 
+       if (age1 >= age2) {
+        return acc;
+       } else {
+        return cur;
+       }
+    });
 };
 
 // Do not edit below this line
